8466c91652556ef889fa5b97b40fe115dbf69014
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-02-08 18:01:56 -0300

    Corrige imagem do palhaço na seção Quem somos

--- src/features/home/sections/AboutSection.jsx
+++ src/features/home/sections/AboutSection.jsx
@@ -1,11 +1,14 @@
-import { Container, Typography } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 
 export function AboutSection() {
   return (
     <Container id="quem-somos" maxWidth="lg" className="summary-band section-alt-gray centered animate__animated animate__fadeInUp page-container" style={{ '--animate-duration': '750ms' }}>
-      <Typography component="p" variant="body1" className="about-message-inline">
-        “Somos a Carliz doces e realizamos doces a pronta entrega para festas, casamentos, aniversários e ovos de páscoa.”
-      </Typography>
+      <Box className="about-clown-dialog">
+        <Box component="img" src="/images/tela-apresentacao/palhaco.png" alt="Palhaço da Carliz Doces" className="about-clown-image" />
+        <Typography component="p" variant="body1" className="about-message-inline">
+          “Somos a Carliz doces e realizamos doces a pronta entrega para festas, casamentos, aniversários e ovos de páscoa.”
+        </Typography>
+      </Box>
     </Container>
   )
 }
