a6d2b15d27af206dccd124d97248e58ba306f8e2
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-02-08 16:56:08 -0300

    Corrige imagens para PNG sem alterar arquivos binários

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -305,7 +305,7 @@ export function HomePage() {
           <Box className="intro-center-content">
             <Box component="img" src="/images/logo-carlizdoces.jpg" alt="Logo da Carliz Doces" className="intro-logo" />
             <Box className="intro-clown-wrap">
-              <Box component="img" src="/images/novo-palhaco.png" alt="Palhaços anunciando o espetáculo" className="intro-clown" />
+              <Box component="img" src="/images/palhaco.png" alt="Palhaços anunciando o espetáculo" className="intro-clown" />
               <Box component="p" className="intro-message">Respeitável publico apresentamos Carliz Doces</Box>
             </Box>
           </Box>
