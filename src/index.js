1e9b6b7af10efac7c38068b1a71d80fbc9f0557e
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-02-18 03:28:35 -0300

    Fix app entry to render Home page router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './animate.css'
+import './index.css'
+import App from './App.jsx'
+import { AppProviders } from './app/providers/AppProviders'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>,
+)
