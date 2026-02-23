d825bc8111fac115d58e68c40dab2b521e6dc4cb
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-02-23 01:40:31 -0300

    Atualiza link do vídeo no modal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,8 +23,8 @@ const InstagramSection = lazy(() => import('../components/sections/InstagramSect
 const UpdatesSection = lazy(() => import('../components/sections/UpdatesSection'))
 const MotionDiv = motion.div
 const STORE_LIKES_ITEM_ID = 'store'
-const FEATURED_VIDEO_EMBED_URL = 'https://www.youtube.com/embed/kXq-YwNHjqA'
-const FEATURED_VIDEO_FALLBACK_URL = 'https://youtube.com/shorts/kXq-YwNHjqA?feature=share'
+const FEATURED_VIDEO_EMBED_URL = 'https://www.youtube.com/embed/FezN9hhSSxw'
+const FEATURED_VIDEO_FALLBACK_URL = 'https://youtube.com/shorts/FezN9hhSSxw?feature=share'
 const featuredVideoDecorations = [
   { icon: '🍬', top: -34, left: 20, delay: '0s', duration: '4.5s', size: { xs: '1.8rem', sm: '2.1rem' } },
   { icon: '🍭', top: -36, left: '24%', delay: '0.5s', duration: '5.2s', size: { xs: '1.9rem', sm: '2.2rem' } },
