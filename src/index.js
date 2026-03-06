20f52ad4ef8651fe9e326e1b14f3c27752252ddd
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-03-06 11:02:12 -0300

    style: deixa 404 mais colorida com tema de doces

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { AppProviders } from './app/providers/AppProviders'
 import { BrowserRouter } from 'react-router-dom'
+import { AppErrorBoundary } from './app/errors/AppErrorBoundary'
+import { redirectTo404 } from './app/errors/redirectTo404'
+
+window.addEventListener('error', redirectTo404)
+window.addEventListener('unhandledrejection', redirectTo404)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProviders>
-        <App />
-      </AppProviders>
+      <AppErrorBoundary>
+        <AppProviders>
+          <App />
+        </AppProviders>
+      </AppErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>,
 )
